522c15f8e187806273abac16acafe66218c73bff
张张涛 <zhangzhangtao@zhangzhtaodeMBP.lan>
2018-08-27 01:24:28 +0800

    mongodb init

--- src/components/banner-component.vue
+++ src/components/banner-component.vue
@@ -1,5 +1,6 @@
 <style>
-  .banner-component>.margin{height: 460px;background-color: #666}
+  .banner-component>.margin{height: 460px;background-color: #666;background-image: url('https://i1.mifile.cn/a4/xmad_15067770206999_vqbxD.jpg')}
+  
 </style>
 
 <template>
--- src/components/category-list.vue
+++ src/components/category-list.vue
@@ -5,150 +5,127 @@
     font-size:16px;
     line-height:60px;
     display: block;
-    /* border: 1px solid salmon */
+  }
+  .category-list>.category-list-content{display: flex;font-size: 16px;position: absolute;top: 60px;left: 0;}
+  .category-list>.category-list-content>ul{background-color: rgba(0, 0, 0, 0.6);padding: 20px 0 20px 0;}
+  .category-list>.category-list-content>ul>li{width: 204px;line-height: 42px;padding-left: 30px;color: #fff}
+  .category-list>.category-list-content>div{height: 460px;}
+  .category-list>.category-list-content>div>ul{
+    border-right: 1px solid saddlebrown;
+    height: 460px;
+    display: flex;
+    flex-wrap:wrap;
+    flex-direction: column;
+    align-content: flex-start;
+    background-color: #fff
+  }
+  .category-list>.category-list-content>div>ul>li{
+    width: 245px;
+    height: 76px;
+    display: flex;
+    align-items: center;
+    padding-left: 20px;
+    font-size: 14px;
+    color: #333
     }
-    .category-list>.category-list-content{display: flex;font-size: 16px;position: absolute;top: 60px;left: 0;}
-    .category-list>.category-list-content>ul{background-color: rgba(0, 0, 0, 0.6);padding: 20px 0 20px 0;}
-    .category-list>.category-list-content>ul>li{width: 204px;line-height: 42px;padding-left: 30px;color: #fff}
-    .category-list>.category-list-content>ul>li:hover{background-color: #ff6700}
-
-    .category-list>.category-list-content>div{height: 460px;background-color: #fff;}
-    /* .category-list>.category-list-content>div>ul{border: 1px solid crimson} */
-    .category-list>.category-list-content>div>ul{
-      width: 992px;
-      border-right: 1px solid saddlebrown;
-      height: 460px;
-      display: flex;
-      flex-wrap:wrap;
-      flex-direction: column;
-      align-content: flex-start
-      }
-    .category-list>.category-list-content>div>ul>li{
-      width: 245px;
-      height: 76px;
-      display: flex;
-      align-items: center;
-      padding-left: 20px;
-      font-size: 14px;
-      color: #333
-      }
-    .category-list>.category-list-content>div>ul>li>img{width: 40px;height: 40px;margin-right: 12px}
+  .category-list>.category-list-content>div>ul>li:hover{color: #ff6700}
+  .category-list>.category-list-content>div>ul>li>img{width: 40px;height: 40px;margin-right: 12px}
 </style>
 <template>
   <div class="category-list margin">
     <span class="category-list-title iconfont icon-list">全部商品分类</span>
-    <div class="category-list-content">
+    <div class="category-list-content"
+    @mouseleave="closeAllActiveLargeclass">
       <ul class="ul-li_a">
-        <li v-for="(item,index) of virtualLargeclass" :key="index" 
+        <li v-for="(virtualLargeclass,index) of virtualLargeclasses" :key="index" 
           @mousemove="triggerActiveLargeclass(index)"
-          @mouseleave="closeAllActiveLargeclass">
-          {{item}}
+          :style="{'background-color': activeLargeColor[index]}">
+          {{virtualLargeclass}}
         </li>
       </ul>
       <div>
-        <ul class="ul-li_a" 
-          v-for="(item,index) of virtualLargeclass" 
-          :key="index" v-show="activeLargeclass[index]"
-          @mousemove="fn(index)">
-          <li  v-for="(item_li,index) of virtualData" :key="index" v-if="item==item_li.largeclass">
+        <ul class="ul-li_a"
+          :style="{'width':activeLargeclassWidth[index]}" 
+          v-for="(virtualLargeclass,index) of virtualLargeclasses" 
+          :key="index" 
+          v-show="activeLargeclass[index]"
+          @mouseleave="closeAllActiveLargeclass">
+          <li 
+            v-for="(virtualData,index) of virtualDatas" 
+            :key="index" 
+            v-if="virtualLargeclass==virtualData.largeclass">
             <img src="https://i1.mifile.cn/f/i/g/2015/cn-index/m8-80.png?width=80&height=80" alt="">
-            <span>{{item_li.smallclass}}</span>
+            <span>{{virtualData.smallclass}}</span>
           </li>
-          
         </ul>
       </div>
     </div>
   </div>
 </template>
 <script>
+  import axios from 'axios'
   export default {
     data(){
       return{
-        virtualLargeclass:['红茶','绿茶','白茶','黑茶','乌龙茶'],
-        virtualData:[
-          {'largeclass': '红茶','smallclass':'红茶0'},
-          {'largeclass': '红茶','smallclass':'红茶1'},
-          {'largeclass': '红茶','smallclass':'红茶2'},
-          {'largeclass': '红茶','smallclass':'红茶3'},
-          {'largeclass': '红茶','smallclass':'红茶4'},
-          {'largeclass': '红茶','smallclass':'红茶5'},
-          {'largeclass': '红茶','smallclass':'红茶6'},
-          {'largeclass': '红茶','smallclass':'红茶7'},
-          {'largeclass': '红茶','smallclass':'红茶8'},
-          {'largeclass': '红茶','smallclass':'红茶9'},
-
-          {'largeclass': '红茶','smallclass':'红茶5'},
-          {'largeclass': '红茶','smallclass':'红茶6'},
-          {'largeclass': '红茶','smallclass':'红茶7'},
-          {'largeclass': '红茶','smallclass':'红茶8'},
-          {'largeclass': '红茶','smallclass':'红茶9'},
-
-          {'largeclass': '绿茶','smallclass':'绿茶0'},
-          {'largeclass': '绿茶','smallclass':'绿茶1'},
-          {'largeclass': '绿茶','smallclass':'绿茶2'},
-          {'largeclass': '绿茶','smallclass':'绿茶3'},
-          {'largeclass': '绿茶','smallclass':'绿茶4'},
-          {'largeclass': '绿茶','smallclass':'绿茶5'},
-          {'largeclass': '绿茶','smallclass':'绿茶6'},
-          {'largeclass': '绿茶','smallclass':'绿茶7'},
-          {'largeclass': '绿茶','smallclass':'绿茶8'},
-          {'largeclass': '绿茶','smallclass':'绿茶9'},
-
-          {'largeclass': '白茶','smallclass':'白茶0'},
-          {'largeclass': '白茶','smallclass':'白茶1'},
-          {'largeclass': '白茶','smallclass':'白茶2'},
-          {'largeclass': '白茶','smallclass':'白茶3'},
-          {'largeclass': '白茶','smallclass':'白茶4'},
-          {'largeclass': '白茶','smallclass':'白茶5'},
-          {'largeclass': '白茶','smallclass':'白茶6'},
-          {'largeclass': '白茶','smallclass':'白茶7'},
-          {'largeclass': '白茶','smallclass':'白茶8'},
-          {'largeclass': '白茶','smallclass':'白茶9'},
-
-          {'largeclass': '黑茶','smallclass':'黑茶0'},
-          {'largeclass': '黑茶','smallclass':'黑茶1'},
-          {'largeclass': '黑茶','smallclass':'黑茶2'},
-          {'largeclass': '黑茶','smallclass':'黑茶3'},
-          {'largeclass': '黑茶','smallclass':'黑茶4'},
-          {'largeclass': '黑茶','smallclass':'黑茶5'},
-          {'largeclass': '黑茶','smallclass':'黑茶6'},
-          {'largeclass': '黑茶','smallclass':'黑茶7'},
-          {'largeclass': '黑茶','smallclass':'黑茶8'},
-          {'largeclass': '黑茶','smallclass':'黑茶9'},
-
-          {'largeclass': '乌龙茶','smallclass':'乌龙茶0'},
-          {'largeclass': '乌龙茶','smallclass':'乌龙茶1'},
-          {'largeclass': '乌龙茶','smallclass':'乌龙茶2'},
-          {'largeclass': '乌龙茶','smallclass':'乌龙茶3'},
-          {'largeclass': '乌龙茶','smallclass':'乌龙茶4'},
-          {'largeclass': '乌龙茶','smallclass':'乌龙茶5'},
-          {'largeclass': '乌龙茶','smallclass':'乌龙茶6'},
-          {'largeclass': '乌龙茶','smallclass':'乌龙茶7'},
-          {'largeclass': '乌龙茶','smallclass':'乌龙茶8'},
-          {'largeclass': '乌龙茶','smallclass':'乌龙茶9'},
-        ],
-        activeLargeclass:[false,false,false,false,false]
+        virtualLargeclasses:['红茶','绿茶','白茶','黑茶','乌龙茶','花茶','黄茶','药茶','茶具','其它'],
+        virtualDatas:[],
+        activeLargeclass: Array(10).fill(false),
+        activeLargeColor:Array(10).fill('rgba(0, 0, 0, 0)')
       }
+    },
+    mounted(){
+      let _this = this
+      axios.get('http://101.132.46.146:3002/test')
+      .then(function (response) {
+        _this.virtualDatas = response.data
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     },
     methods:{
       triggerActiveLargeclass(index){
-        this.activeLargeclass = [false,false,false,false,false];
+        this.activeLargeclass = Array(10).fill(false)
+        this.activeLargeColor = Array(10).fill('rgba(0, 0, 0, 0)');
         this.activeLargeclass[index] = true;
+        this.activeLargeColor[index] = '#ff6700'
       },
       closeAllActiveLargeclass(){
-        setTimeout(()=>{
-          this.activeLargeclass = [false,false,false,false,false];
-        },1000)
-        
-      },
-      fn(index){
-        // setTimeout
-
-
-        setTimeout(()=>{
-          this.activeLargeclass[index] = true;
-          console.log(1111)
-        },1100)
+        this.activeLargeclass = Array(10).fill(false)
+        this.activeLargeColor = Array(10).fill('rgba(0, 0, 0, 0)');
+      }
+    },
+    computed:{
+      activeLargeclassWidth(){
+        let datashuliang = Array(10).fill(0)
+        for(let i in this.virtualDatas){
+          if(this.virtualDatas[i]['largeclass'] == "红茶"){
+            datashuliang[0]++
+          }else if(this.virtualDatas[i]['largeclass'] == "绿茶"){
+            datashuliang[1]++
+          }else if(this.virtualDatas[i]['largeclass'] == "白茶"){
+            datashuliang[2]++
+          }else if(this.virtualDatas[i]['largeclass'] == "黑茶"){
+            datashuliang[3]++
+          }else if(this.virtualDatas[i]['largeclass'] == "乌龙茶"){
+            datashuliang[4]++
+          }else if(this.virtualDatas[i]['largeclass'] == "花茶"){
+            datashuliang[5]++
+          }else if(this.virtualDatas[i]['largeclass'] == "黄茶"){
+            datashuliang[6]++
+          }else if(this.virtualDatas[i]['largeclass'] == "药茶"){
+            datashuliang[7]++
+          }else if(this.virtualDatas[i]['largeclass'] == "茶具"){
+            datashuliang[8]++
+          }else if(this.virtualDatas[i]['largeclass'] == "其它"){
+            datashuliang[9]++
+          }else{}
+        }
+        let activeLargeclassWidth = []
+        for(let i in datashuliang){
+          activeLargeclassWidth.push(Math.ceil(datashuliang[i]/6)*245+'px')
+        }
+        return activeLargeclassWidth
       }
     }
   }
